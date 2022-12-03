74fcb785f15bb8d3665110a24c7a69f9fb33f5c2
Sam <sambpilot@gmail.com>
2022-12-03 09:48:53 +0000

    isPalindrome feature

--- Main.hs
+++ Main.hs
@@ -5,6 +5,12 @@ main = do
   input <- getLine
   let x = (read input :: Int)
   print(tripleThis x)
+  putStrLn "Please enter in a string to check if it is a palindrome"
+  temp <- getLine
+  print(isPalindrome temp)
 
 tripleThis :: Int -> Int
 tripleThis n = n * 3
+
+isPalindrome :: String -> Bool
+isPalindrome x = x == reverse x
